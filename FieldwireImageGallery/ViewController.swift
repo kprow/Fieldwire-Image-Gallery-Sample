7cf1dca6a98aa43b7f60d1948446faff8cad50c3
Andrew Koprowski <kprow@Andrews-MacBook-Pro.local>
2024-10-15 21:08:39 -0600

    Initial creation of Xcode Project

---
+++ FieldwireImageGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FieldwireImageGallery
+//
+//  Created by Andrew Koprowski on 10/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
